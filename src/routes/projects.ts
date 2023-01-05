7aa9d9a7f7faf9b947e4dc3f645dcd2258e1cfb3
unknown <juanignacio.delossantos@4rsoluciones.com>
2023-01-05 18:30:35 -0300

    create endpoint create project

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import createProject from "../controllers/projects/createProject";
 import getProjects from "../controllers/projects/getProjects";
 
 const projectRoute = Router();
 
 projectRoute.get("/", getProjects);
+projectRoute.post("/create", createProject);
 
 export default projectRoute;
