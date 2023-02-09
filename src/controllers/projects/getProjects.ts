7d71a062d5a4c8c18043cb36ba47718a12e30da5
unknown <juanignacio.delossantos@4rsoluciones.com>
2023-02-09 19:31:10 -0300

    start implement pagination

--- src/controllers/projects/getProjects.ts
+++ src/controllers/projects/getProjects.ts
@@ -3,13 +3,33 @@ import { Project } from "../../entities";
 import { AppDataSource } from "../../data-source";
 
 const getProjects = async (req: Request, res: Response) => {
+  const pageSize: number = req.query.pageSize ? Number(req.query.pageSize) : 5;
+  const currentPage: number = req.query.currentPage
+    ? Number(req.query.currentPage)
+    : 1;
+  const offset: number = currentPage * pageSize - pageSize;
   try {
+    console.log(offset);
+    const totalProjects = await AppDataSource.getRepository(Project)
+      .createQueryBuilder("project")
+      .getCount();
+
     const projectCategory = await AppDataSource.getRepository(Project)
       .createQueryBuilder("project")
+      .limit(offset + pageSize)
+      .offset(offset)
       .leftJoinAndSelect("project.category", "category")
       .getMany();
 
-    return res.status(200).json(projectCategory);
+    console.log(totalProjects);
+
+    if (projectCategory.length > 0) {
+      const projectQuantity = projectCategory.length;
+      return res
+        .status(200)
+        .json({ data: projectCategory, totalItems: totalProjects });
+    }
+    return res.status(200).json({ message: "No project found" });
   } catch (err) {
     if (err instanceof Error) res.status(400).json({ message: err.message });
     else res.status(400).json({ error: err, message: "Unexpected error" });
